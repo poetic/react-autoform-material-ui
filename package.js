1efd23215814968967b3cfcb74772c25572e593a
codeNpaper <kenoduah@poeticsystems.com>
2015-08-14 14:43:48 -0500

    modify 0.0.1 package.js. Update package summary

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'poetic:react-autoform-material-ui',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'React-Autoform-MaterialUi, is a meteor package for Autoform that helps render your forms using Material-UI components',
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/poetic/react-autoform-material-ui',
     // By default, Meteor will default to using README.md for documentation.
